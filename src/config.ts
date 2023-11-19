b0149d120ce3f1d103e7c702a377350149bd9459
wu-xiaowei <496384622@qq.com>
2023-11-19 16:56:39 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,7 @@ export interface IConfig {
 export const config: IConfig = {
   mjProxyEndpoint: process.env.MJ_PROXY_ENDPOINT || "http://localhost:8022/mj",
   notifyHook: process.env.MJ_NOFIFY_HOOK || "http://localhost:4120/notify",
-  httpProxy: process.env.HTTP_PROXY || "",
+  // httpProxy: process.env.HTTP_PROXY || "",
+  httpProxy: "http://127.0.0.1:7890",
   imagesPath: process.env.IMAGE_PATH || ""
 }
